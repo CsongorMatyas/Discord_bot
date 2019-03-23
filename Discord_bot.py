e0a91f0ee22afa2a4f9b4aca2940b38fe01e427a
Csongor Matyas <cfm482@mun.ca>
2019-03-23 17:28:57 -0230

    Formattin text

--- Discord_bot.py
+++ Discord_bot.py
@@ -10,28 +10,30 @@ import discord
 client = discord.Client()
 
 helpMessage = ('''
-    !help          --> Brings up this message
-    !ping          --> To test is bot is online and working, 
-                       it will send "<@userID> Pong!" as response
-    ?hg            --> Prints High Growth related information
-    ?levelup       --> Prints Level-up related information
-    ?map           --> Prints map general starting positions
-    ?events        --> Prints all events
-    ?events 4      --> Prints 4th event
-    ?events -10    --> Prints last 10 events
-    ?heroes        --> Prints a list of heroes
-    ?heroes nature --> Prints a list of nature heroes 
-                       similarly (order, chaos, ranged, melee, building,
-                       mental, shield, evasion, block, poison, freeze,
-                       fly, damage, death, rune, attack, health, heal,
-                       pierce, summon, kill, attacked, immune, transform, 
-                       silence, common, rare, epic, legendary) 
-                       or multiple keywords separated by (" ", ",", ";")
-    ?hero death    --> Prints information regarding the hero
-    ?pit           --> Prints table of Pit bosses vs league
-    ?elixir        --> Elixir usage - How much will give how many
-    ?guild doh     --> Information about the guild
-    ''')
+    !
+help          --> Brings up this message
+ping          --> To test is bot is online and working, 
+                  it will send "<@userID> Pong!" as response
+?
+hg            --> Prints High Growth related information
+levelup       --> Prints Level-up related information
+map           --> Prints map general starting positions
+events        --> Prints all events
+events 4      --> Prints 4th event
+events -10    --> Prints last 10 events
+heroes        --> Prints a list of heroes
+heroes nature --> Prints a list of nature heroes 
+                  similarly (order, chaos, ranged, melee, building,
+                  mental, shield, evasion, block, poison, freeze,
+                  fly, damage, death, rune, attack, health, heal,
+                  pierce, summon, kill, attacked, immune, transform, 
+                  silence, common, rare, epic, legendary) 
+                  or multiple keywords separated by (" ", ",", ";")
+hero death    --> Prints information regarding the hero
+pit           --> Prints table of Pit bosses vs league
+elixir        --> Elixir usage - How much will give how many
+guild doh     --> Information about the guild
+''')
 
 @client.event
 async def on_ready():
